046d0a1769fd2f730716c38ac28c55e2dd044e08
Anne Lin <annelin@Annes-MacBook-Pro.local>
2020-04-08 02:15:35 -0400

    docs

---
+++ docs/precache-manifest.91a9c552138b7fd2a5d9f5269897c503.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9dc6b0cf9942fcbba29d656efa4dea9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dce7b20c76cc8701d2e1",
+    "url": "/static/css/main.a4f97f1e.chunk.css"
+  },
+  {
+    "revision": "b5759a975d426953e649",
+    "url": "/static/js/2.fccea8bc.chunk.js"
+  },
+  {
+    "revision": "c44b26629348768bcdbb85f89189bde4",
+    "url": "/static/js/2.fccea8bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce7b20c76cc8701d2e1",
+    "url": "/static/js/main.fad73494.chunk.js"
+  },
+  {
+    "revision": "fcd8744db4f329d8a96c",
+    "url": "/static/js/runtime-main.ac6f0821.js"
+  },
+  {
+    "revision": "c889d8d415775901fdd3497a0773d33c",
+    "url": "/static/media/clark-matching.c889d8d4.mp4"
+  },
+  {
+    "revision": "498f73ac48e7f3d49407f26b18987b9c",
+    "url": "/static/media/domio-dnd.498f73ac.mp4"
+  },
+  {
+    "revision": "b6b034b61c23412f1e74ea0172301e42",
+    "url": "/static/media/homepic1.b6b034b6.jpg"
+  }
+]);
